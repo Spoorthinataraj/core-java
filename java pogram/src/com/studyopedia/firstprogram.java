3bd922b24d19af5e235dcf901ba8b1094595247b
HP <HP@DESKTOP-TFQJFDA>
2024-10-27 05:10:53 +0000

    First commit

---
+++ java pogram/src/com/studyopedia/firstprogram.java
@@ -0,0 +1,14 @@
+package com.studyopedia;
+// program to demonstrate hello world
+/*
+ * multiline 
+ */
+public class firstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
