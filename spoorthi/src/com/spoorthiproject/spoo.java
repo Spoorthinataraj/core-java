f61999188abab213fdb84ce70986731aa444feb0
HP <HP@DESKTOP-TFQJFDA>
2024-10-27 05:32:29 +0000

    First commit

---
+++ spoorthi/src/com/spoorthiproject/spoo.java
@@ -0,0 +1,13 @@
+package com.spoorthiproject;
+// progam to demonstrate good morning
+/*
+ * multiline
+ */
+public class spoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("good morning");
+	}
+
+}
